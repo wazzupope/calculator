c93ea8916acfc47decce420b610411d36d37f357
jpfairhart <johnpaulfairhart@hotmail.com>
2022-12-19 22:27:02 -0800

    use eslint to improve code

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-let num1 = "";
-let num2 = "";
+let num1 = '';
+let num2 = '';
 
 function add(a, b) {
-    return a + b;
-};
+  return a + b;
+}
 
 function subtract(a, b) {
-    return a - b;
-};
+  return a - b;
+}
 
 function multiply(a, b) {
-    return a * b;
-};
+  return a * b;
+}
 
 function divide(a, b) {
-    return a / b;
-};
+  return a / b;
+}
 
 let plus = false;
 let minus = false;
@@ -25,341 +25,319 @@ let div = false;
 let resetDisplay = false;
 let clearDisplay = false;
 
-const addition = document.querySelector("#add");
+const display = document.querySelector('#readout');
+const memory = document.querySelector('#memory');
+
+const addition = document.querySelector('#add');
 function addTF() {
-    plus = true;
-    minus = false;
-    times = false;
-    div = false;
-    num1 = display.textContent;
-    memory.textContent = display.textContent + " + ";
-    resetDisplay = true;
-};
-addition.addEventListener("click", addTF);
+  plus = true;
+  minus = false;
+  times = false;
+  div = false;
+  num1 = display.textContent;
+  memory.textContent = `${display.textContent} + `;
+  resetDisplay = true;
+}
+addition.addEventListener('click', addTF);
 
-const subtraction = document.querySelector("#subtract");
+const subtraction = document.querySelector('#subtract');
 function subtractTF() {
-    plus = false;
-    minus = true;
-    times = false;
-    div = false;
-    num1 = display.textContent;
-    memory.textContent = display.textContent + " - ";
-    resetDisplay = true;
-};
-subtraction.addEventListener("click", subtractTF);
+  plus = false;
+  minus = true;
+  times = false;
+  div = false;
+  num1 = display.textContent;
+  memory.textContent = `${display.textContent} - `;
+  resetDisplay = true;
+}
+subtraction.addEventListener('click', subtractTF);
 
-const multiplication = document.querySelector("#multiply");
+const multiplication = document.querySelector('#multiply');
 function multiplyTF() {
-    plus = false;
-    minus = false;
-    times = true;
-    div = false;
-    num1 = display.textContent;
-    memory.textContent = display.textContent + " x ";
-    resetDisplay = true;
-};
-multiplication.addEventListener("click", multiplyTF);
+  plus = false;
+  minus = false;
+  times = true;
+  div = false;
+  num1 = display.textContent;
+  memory.textContent = `${display.textContent} x `;
+  resetDisplay = true;
+}
+multiplication.addEventListener('click', multiplyTF);
 
-const division = document.querySelector("#divide");
+const division = document.querySelector('#divide');
 function divideTF() {
-    plus = false;
-    minus = false;
-    times = false;
-    div = true;
-    num1 = display.textContent;
-    memory.textContent = display.textContent + " / ";
-    resetDisplay = true;
-};
-division.addEventListener("click", divideTF);
+  plus = false;
+  minus = false;
+  times = false;
+  div = true;
+  num1 = display.textContent;
+  memory.textContent = `${display.textContent} / `;
+  resetDisplay = true;
+}
+division.addEventListener('click', divideTF);
 
-const equality = document.querySelector("#equality");
+const equality = document.querySelector('#equality');
 function equals() {
-    clearDisplay = true;
-    let answer;
-    num2 = display.textContent;
-    memory.textContent += num2;
-    switch (true) {
-        case plus:
-            answer = add(Number(num1), Number(num2));
-            display.textContent = answer;
-            memory.textContent += ` = ${answer}`;
-            break;
-        case minus:
-            answer = subtract(Number(num1), Number(num2));
-            display.textContent = answer;
-            memory.textContent += ` = ${answer}`;
-            break;
-        case times:
-            answer = multiply(Number(num1), Number(num2));
-            display.textContent = answer;
-            memory.textContent += ` = ${answer}`;
-            break;
-        case div:
-            answer = divide(Number(num1), Number(num2));
-            display.textContent = answer;
-            memory.textContent += ` = ${answer}`;
-            break;
-        default:
-            break;
-    };
-};
-equality.addEventListener("click", equals);
+  clearDisplay = true;
+  let answer;
+  num2 = display.textContent;
+  memory.textContent += num2;
+  switch (true) {
+    case plus:
+      answer = add(Number(num1), Number(num2));
+      display.textContent = answer;
+      memory.textContent += ` = ${answer}`;
+      break;
+    case minus:
+      answer = subtract(Number(num1), Number(num2));
+      display.textContent = answer;
+      memory.textContent += ` = ${answer}`;
+      break;
+    case times:
+      answer = multiply(Number(num1), Number(num2));
+      display.textContent = answer;
+      memory.textContent += ` = ${answer}`;
+      break;
+    case div:
+      answer = divide(Number(num1), Number(num2));
+      display.textContent = answer;
+      memory.textContent += ` = ${answer}`;
+      break;
+    default:
+      break;
+  }
+}
+equality.addEventListener('click', equals);
 
-const display = document.querySelector("#readout");
-const memory = document.querySelector("#memory");
-
-const clear = document.querySelector("#c");
+const clear = document.querySelector('#c');
 function runClear() {
-    display.textContent = "0";
-    memory.textContent = "";
-    plus = false;
-    minus = false;
-    times = false;
-    div = false;
-    resetDisplay = false;
-};
-clear.addEventListener("click", runClear);
+  display.textContent = '0';
+  memory.textContent = '';
+  plus = false;
+  minus = false;
+  times = false;
+  div = false;
+  resetDisplay = false;
+}
+clear.addEventListener('click', runClear);
 
-const zero = document.querySelector("#zero");
+const zero = document.querySelector('#zero');
 function runZero() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "0";
-    }
-    else {
-        display.textContent += "0";
-    };
-};
-zero.addEventListener("click", runZero);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '0';
+  } else {
+    display.textContent += '0';
+  }
+}
+zero.addEventListener('click', runZero);
 
-const one = document.querySelector("#one");
+const one = document.querySelector('#one');
 function runOne() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "1";
-    }
-    else {
-        display.textContent += "1";
-    };
-};
-one.addEventListener("click", runOne);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '1';
+  } else {
+    display.textContent += '1';
+  }
+}
+one.addEventListener('click', runOne);
 
-const two = document.querySelector("#two");
+const two = document.querySelector('#two');
 function runTwo() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "2";
-    }
-    else {
-        display.textContent += "2";
-    };
-};
-two.addEventListener("click", runTwo);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '2';
+  } else {
+    display.textContent += '2';
+  }
+}
+two.addEventListener('click', runTwo);
 
-const three = document.querySelector("#three");
+const three = document.querySelector('#three');
 function runThree() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "3";
-    }
-    else {
-        display.textContent += "3";
-    };
-};
-three.addEventListener("click", runThree);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '3';
+  } else {
+    display.textContent += '3';
+  }
+}
+three.addEventListener('click', runThree);
 
-const four = document.querySelector("#four");
+const four = document.querySelector('#four');
 function runFour() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "4";
-    }
-    else {
-        display.textContent += "4";
-    };
-};
-four.addEventListener("click", runFour);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '4';
+  } else {
+    display.textContent += '4';
+  }
+}
+four.addEventListener('click', runFour);
 
-const five = document.querySelector("#five");
+const five = document.querySelector('#five');
 function runFive() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "5";
-    }
-    else {
-        display.textContent += "5";
-    };
-};
-five.addEventListener("click", runFive);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '5';
+  } else {
+    display.textContent += '5';
+  }
+}
+five.addEventListener('click', runFive);
 
-const six = document.querySelector("#six");
+const six = document.querySelector('#six');
 function runSix() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "6";
-    }
-    else {
-        display.textContent += "6";
-    };
-};
-six.addEventListener("click", runSix);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '6';
+  } else {
+    display.textContent += '6';
+  }
+}
+six.addEventListener('click', runSix);
 
-const seven = document.querySelector("#seven");
+const seven = document.querySelector('#seven');
 function runSeven() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "7";
-    }
-    else {
-        display.textContent += "7";
-    };
-};
-seven.addEventListener("click", runSeven);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '7';
+  } else {
+    display.textContent += '7';
+  }
+}
+seven.addEventListener('click', runSeven);
 
-const eight = document.querySelector("#eight");
+const eight = document.querySelector('#eight');
 function runEight() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "8";
-    }
-    else {
-        display.textContent += "8";
-    };
-};
-eight.addEventListener("click", runEight);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '8';
+  } else {
+    display.textContent += '8';
+  }
+}
+eight.addEventListener('click', runEight);
 
-const nine = document.querySelector("#nine");
+const nine = document.querySelector('#nine');
 function runNine() {
-    if (clearDisplay) {
-        display.textContent = "0";
-        memory.textContent = "";
-        clearDisplay = false;
-    }
-    else if (resetDisplay) {
-        display.textContent = "0";
-        resetDisplay = false;
-    };
-    if (display.textContent === "0") {
-        display.textContent = "9";
-    }
-    else {
-        display.textContent += "9";
-    };
-};
-nine.addEventListener("click", runNine);
+  if (clearDisplay) {
+    display.textContent = '0';
+    memory.textContent = '';
+    clearDisplay = false;
+  } else if (resetDisplay) {
+    display.textContent = '0';
+    resetDisplay = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '9';
+  } else {
+    display.textContent += '9';
+  }
+}
+nine.addEventListener('click', runNine);
 
-const period = document.querySelector("#decimal");
+const period = document.querySelector('#decimal');
 function runPeriod() {
-    display.textContent += ".";
-};
-period.addEventListener("click", runPeriod);
+  display.textContent += '.';
+}
+period.addEventListener('click', runPeriod);
 
-const back = document.querySelector("#back");
+const back = document.querySelector('#back');
 function runBack() {
-    if (display.textContent.length === 1) {
-        display.textContent = "0";
-    }
-    else {
-        let stringArray = display.textContent.split("");
-        stringArray = stringArray.slice(0, (stringArray.length - 1));
-        stringArray = stringArray.join("");
-        display.textContent = stringArray;
-    };
-};
-back.addEventListener("click", runBack);
+  if (display.textContent.length === 1) {
+    display.textContent = '0';
+  } else {
+    let stringArray = display.textContent.split('');
+    stringArray = stringArray.slice(0, (stringArray.length - 1));
+    stringArray = stringArray.join('');
+    display.textContent = stringArray;
+  }
+}
+back.addEventListener('click', runBack);
 
-const plusMinus = document.querySelector("#plusMinus");
+const plusMinus = document.querySelector('#plusMinus');
 function posNeg() {
-    let letterArray = display.textContent.split("");
-    if (letterArray[0] === "-") {
-        letterArray.shift();
-    }
-    else {
-        letterArray.unshift("-");
-    };
-    letterArray = letterArray.join("");
-    display.textContent = letterArray;
-};
-plusMinus.addEventListener("click", posNeg);
-
+  let letterArray = display.textContent.split('');
+  if (letterArray[0] === '-') {
+    letterArray.shift();
+  } else {
+    letterArray.unshift('-');
+  }
+  letterArray = letterArray.join('');
+  display.textContent = letterArray;
+}
+plusMinus.addEventListener('click', posNeg);
+/*
 function textButton(button) {
-    let id = button.id;
-    display.textContent + id;
-};
-
+  const { id } = button;
+  display.textContent + id;
+}
+*/
 /*
 How to get rid of redundancy of code for each number button:
 operatorButtons.forEach((button) =>
